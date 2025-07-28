41f5b957b8e8e30bedaafc68c0e4c6e64985e8fc
acabr017 <arielcabrera3@gmail.com>
2025-07-27 21:40:34 -0400

    trying another way to deploy.

--- backend/build.sh
+++ backend/build.sh
@@ -1,7 +1,7 @@
 set -o errexit
 
-pip install -r backend/requirements.txt
+pip install -r requirements.txt
 
-python backend/manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
-python backend/manage.py migrate
+python manage.py migrate
